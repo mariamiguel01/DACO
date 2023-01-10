f8beccc5a143a17e288c11bcff0518879073fae8
InesAMar <ines.alves.martins01@gmail.com>
2023-01-10 18:16:18 +0000

    k tests

--- KNN.py
+++ KNN.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from PIL import Image
-import tqdm
+from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 
 
@@ -17,31 +17,33 @@ x_train, x_eval, y_train, y_eval = train_test_split(
     x, y, stratify=y, test_size=0.25 #TESTAR o stratify
 )
 
+k_test = [12,15,18,25,30]
+
 #Step-1: Select the number K of the neighbors
-K = 5
-#Step-2: Calculate the Euclidean distance of K number of neighbors
-all_distances = []
-all_prob = []
-predictions = []
-for sample in x_eval:
-    distance = []
-    for x in x_train:
-        distance.append(sum((sample - x)**2))
-    sorted_distances_index = np.argsort(distance)[::-1][:K]
-    all_distances.append(sorted_distances_index)
+for K in k_test:
+    #Step-2: Calculate the Euclidean distance of K number of neighbors
+    all_distances = []
+    all_prob = []
+    predictions = []
+    for sample in tqdm(x_eval):
+        distance = []
+        for x in x_train:
+            distance.append(sum((sample - x)**2))
+        sorted_distances_index = np.argsort(distance)[::-1][:K]
+        all_distances.append(sorted_distances_index)
 
-    sample_prob = []
-    for category in range(8):
-        sample_prob.append(sum(y_train[sorted_distances_index] == category)/K)
-    all_prob.append(sample_prob)
+        sample_prob = []
+        for category in range(8):
+            sample_prob.append(sum(y_train[sorted_distances_index] == category)/K)
+        all_prob.append(sample_prob)
 
-    predictions.append(np.argmax(sample_prob))
+        predictions.append(np.argmax(sample_prob))
 
-#Step-3: Take the K nearest neighbors as per the calculated Euclidean distance.
-#Step-4: Among these k neighbors, count the number of the data points in each category.
-#Step-5: Assign the new data points to that category for which the number of the neighbor is maximum.
-#Step-6: Our model is ready.
+    #Step-3: Take the K nearest neighbors as per the calculated Euclidean distance.
+    #Step-4: Among these k neighbors, count the number of the data points in each category.
+    #Step-5: Assign the new data points to that category for which the number of the neighbor is maximum.
+    #Step-6: Our model is ready.
 
-# Evaluate the accuracy of the classifier
-accuracy = np.mean(predictions == y_eval)*100
-print(f'Accuracy: {accuracy:.2f}')
+    # Evaluate the accuracy of the classifier
+    accuracy = np.mean(predictions == y_eval)*100
+    print(K, f' Accuracy: {accuracy:.2f}')
