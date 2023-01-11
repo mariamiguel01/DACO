db940eaf446c4bbafbed064edd1273eab2ad50d7
mariamiguel01 <maria.migbeirao@gmail.com>
2023-01-11 15:58:46 +0000

    parzen

--- Maria_test/parzn_.py
+++ Maria_test/parzn_.py
@@ -6,13 +6,21 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from tqdm import tqdm
 from scipy.stats import gaussian_kde
-
-def kde_sklearn(X_c, x_validate, bandwidth=0.5):
-  kde = KernelDensity(kernel='gaussian',bandwidth=bandwidth).fit(X_c)
-  return kde.score_samples(x_validate)
+from typing import List
+from scipy.stats import multivariate_normal
 
 
-train_features_csv = np.loadtxt('features_file_resnet.csv', delimiter=',')
+def evaluate_gaussian_all(X_c: np.ndarray, x: np.ndarray)->np.ndarray:
+  gaussian_list = []
+  totalvalue=0
+  for i in range(X_c.shape[0]):
+      mu = X_c[i]
+      sigma = np.identity(X_c.shape[1])
+      gaussian = lambda x :multivariate_normal(mu, sigma)
+      gaussian_list.append(gaussian(x))
+  return np.array(gaussian_list).T
+
+train_features_csv = np.loadtxt('features_file_VGG16.csv', delimiter=',')
 train_labels = pd.read_csv("train_labels.csv", index_col="id")
 species_labels = sorted(train_labels.columns.unique())
 class_num=np.size(species_labels)
@@ -20,27 +28,22 @@ class_num=np.size(species_labels)
 x = train_features_csv[:, 1:]
 y = train_features_csv[:,0]
 
+
 x_train, x_validate, y_train, y_validate = train_test_split(
     x, y, stratify=y, test_size=0.1
 )
-print(y_train)
 predictions = []
-
-for i in tqdm(x_validate):
-  p_array = []
+p_array=[]
+pdfs=[]
+for i in range(x_validate.shape[0]):
+  pdfs = []
   for c in tqdm(range(7)):
-      pdfs = []
       X_c = x_train[y_train==c]
-      if(len(X_c)==0):
-        pdf_c=0;
-      else:
-        feature_vec = np.array(X_c)
-        validation=np.array(x_validate)
-        pdf_c = kde_sklearn(feature_vec,validation,bandwidth=0.5)
-      p_array.append(pdf_c)
-  pred = np.max(p_array)
+      x = x_validate[i]
+      pdf_c = evaluate_gaussian_all(X_c, x)
+      pdfs.append(pdf_c)
+  pred=np.argmax(pdfs)
   predictions.append(pred)
-  
 
 # Evaluate the accuracy of the classifier
 accuracy = np.mean(predictions == y_validate)
