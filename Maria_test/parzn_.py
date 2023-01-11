e65c715c039fccf25e77f3aace87db9594ce6196
mariamiguel01 <maria.migbeirao@gmail.com>
2023-01-11 09:18:37 +0000

    Update parzn_.py

--- Maria_test/parzn_.py
+++ Maria_test/parzn_.py
@@ -7,17 +7,9 @@ from PIL import Image
 from tqdm import tqdm
 from scipy.stats import gaussian_kde
 
-def kde_sklearn(X_c, x_grid, bandwidth=0.5):
-  total_value = 0
-  for i in range(len(X_c)):   
-    feature_vec = np.array(X_c)
-    kde = gaussian_kde(feature_vec.T)
-    gaussian = lambda x: kde.evaluate(x)
-    gaussian_atpoint=gaussian(X_c)
-    value=gaussian(x_grid)
-    total_value += value
-    print(len(value))
-  return total_value
+def kde_sklearn(X_c, x_validate, bandwidth=0.5):
+  kde = KernelDensity(kernel='gaussian',bandwidth=bandwidth).fit(X_c)
+  return kde.score_samples(x_validate)
 
 
 train_features_csv = np.loadtxt('features_file_resnet.csv', delimiter=',')
@@ -31,17 +23,22 @@ y = train_features_csv[:,0]
 x_train, x_validate, y_train, y_validate = train_test_split(
     x, y, stratify=y, test_size=0.1
 )
+print(y_train)
 predictions = []
 
 for i in tqdm(x_validate):
   p_array = []
-  for c in tqdm(range(8)):
+  for c in tqdm(range(7)):
       pdfs = []
       X_c = x_train[y_train==c]
-      pdf_c = kde_sklearn(X_c,i,bandwidth=0.5)
-      print(pdf_c)
+      if(len(X_c)==0):
+        pdf_c=0;
+      else:
+        feature_vec = np.array(X_c)
+        validation=np.array(x_validate)
+        pdf_c = kde_sklearn(feature_vec,validation,bandwidth=0.5)
       p_array.append(pdf_c)
-  pred = np.argmax(np.array(p_array))
+  pred = np.max(p_array)
   predictions.append(pred)
   
 
