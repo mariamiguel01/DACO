0df0d1fd96a1b4697ad82a31029860092353bfa9
InesAMar <ines.alves.martins01@gmail.com>
2023-01-15 15:18:33 +0000

    final

--- Deep Learning Methods/CNN.py
+++ Deep Learning Methods/CNN.py
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import metrics
 
+# import feature files
 train_samples_csv = pd.read_csv('Features/features_VGG16_train.csv', delimiter=',')
 test_samples_csv = pd.read_csv('Features/features_VGG16_test.csv', delimiter=',')
 train_labels = pd.read_csv("train_labels.csv", index_col="id")
@@ -21,7 +22,7 @@ y_test = test_samples_csv.iloc[:,1]
 # standardize train features
 scaler = StandardScaler().fit(x)
 scaled_train = scaler.transform(x)
-# split train data into train and validation
+# Split the training samples in train and validation sets 
 sss = StratifiedShuffleSplit(test_size=0.1, random_state=23)
 for train_index, valid_index in sss.split(scaled_train, y):
     X_train, X_valid = scaled_train[train_index], scaled_train[valid_index]
@@ -30,7 +31,7 @@ for train_index, valid_index in sss.split(scaled_train, y):
 nb_features = len(train_samples_csv.columns) - 2 # number of features
 nb_class = 8
 
-# Keras model with one Convolution1D layer
+# Neural Network layers
 model = Sequential()
 model.add(Conv1D(filters=512, kernel_size=3, input_shape=(nb_features,1)))
 model.add(Activation('relu'))
@@ -52,17 +53,18 @@ print("Fit model on training data")
 nb_epoch = 3
 model.fit(X_train, y_train, epochs=nb_epoch, validation_data=(X_valid, y_valid), batch_size=16)
 
-# Evaluate the model on the test data
+# Evaluate the model on the test set
 print("Evaluate on test data")
 results = model.evaluate(x_test, y_test)
 print("test loss, test acc:", results)
 
-# Generate predictions (probabilities -- the output of the last layer) on new data
+# Generate predictions to the test set
 predictions = model.predict(x_test)
 
 Ytrue_test = np.argmax(np.array(y_test.astype(int)), axis=1)
 Ypred = np.argmax(np.array(predictions).astype(int), axis=1)
 
+# Plot the confusion matrix
 confMatrix = metrics.confusion_matrix(Ytrue_test, Ypred, normalize = None)
 display = metrics.ConfusionMatrixDisplay(confusion_matrix = confMatrix)
 display.plot()
--- Machine Learning Methods/KNN.py
+++ Machine Learning Methods/KNN.py
@@ -5,29 +5,31 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from sklearn import metrics
 
-train_samples_csv = pd.read_csv('Features/features_DenseNet121_train.csv', delimiter=',')
-test_samples_csv = pd.read_csv('Features/features_DenseNet121_test.csv', delimiter=',')
+# import feature files
+train_samples_csv = pd.read_csv('Features/features_ResNet50_train.csv', delimiter=',')
+test_samples_csv = pd.read_csv('Features/features_ResNet50_test.csv', delimiter=',')
 
 train_samples = np.array(train_samples_csv)
 x_train_ = train_samples[:, 2:]
 y_train_ = train_samples[:,1]
 
-# Validation 
+# Split the training samples in train and validation sets 
 x_train, x_eval, y_train, y_eval = train_test_split(
     x_train_, y_train_, stratify=y_train_, test_size=0.20
 )
 
-# Test
+# Test samples
 """ test_samples = np.array(test_samples_csv)
 x_test = test_samples[:, 2:]
 y_test = test_samples[:,1] """
 
+# KNN algorithm
 #1: Select the number K of the neighbors
-k_test = [5,8,10]
-#k_test = [5]
+k_test = [5,8,10] #used to train and validation
+#k_test = [5] #used to test
 
 for K in k_test:
-    #2: Calculate the Euclidean distance of K number of neighbors
+    #2: Calculate the Euclidean distances
     all_distances = []
     all_prob = []
     predictions = []
@@ -36,29 +38,30 @@ for K in k_test:
         for x in x_train:
             distance.append(sum((sample - x)**2))
 
-        #3: Take the K nearest neighbors to calculate the Euclidean distance
+        #3: Take the K nearest neighbours to evaluate the volume
         sorted_distances_index = np.argsort(distance)[:K]
         all_distances.append(sorted_distances_index)
 
-        #4: Among these K neighbors, count the number of the data points in each category
+        #4: Among these K neighbours, count the number of the data points in each category
         sample_prob = []
         for category in range(8):
             sample_prob.append(sum(y_train[sorted_distances_index] == category)/K)
         all_prob.append(sample_prob)
 
-        #5: Assign the new data points to the category for which the number of the neighbor is maximum
+        #5: Assign the new data points to the category for which the number of the neighbourhood is maximum
         predictions.append(np.argmax(sample_prob))
 
     # Evaluate the accuracy of the classifier
     accuracy = np.mean(predictions == y_eval)*100
     print(K, f' Accuracy: {accuracy:.2f}')
 
-
+# Code used only for the test:
 """ Ypred = np.array(predictions).astype(int)
 Yvalid = y_test.astype(int)
 
+# Plot the confusion matrix
 confMatrix = metrics.confusion_matrix(Yvalid, Ypred, normalize = None)
 display = metrics.ConfusionMatrixDisplay(confusion_matrix = confMatrix)
 display.plot()
 plt.show()
-plt.title('Confusion Matrix - densenet') """
+plt.title('Confusion Matrix') """
