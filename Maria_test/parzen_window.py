6ae39f2fca8e59cfc5dd522ec3c44da06c19ba8a
mariamiguel01 <maria.migbeirao@gmail.com>
2023-01-15 14:12:58 +0000

    comentado

--- Maria_test/parzen_window.py
+++ Maria_test/parzen_window.py
@@ -1,3 +1,5 @@
+### import necessary libraries
+#the code that is commented should be uncommented if we want to validate the sigma value by testing different ones
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -6,7 +8,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn import metrics
 import matplotlib.pyplot as plt
 
-
+###defining a gaussian function that will create a gaussian for each point of the train set with that class and evaluate de probability of the test set in that gaussian and sum them for all points belonging to a class
 def evaluate_gaussian_all(X_c, x):
   gaussian_list = []
   totalvalue=0
@@ -18,7 +20,7 @@ def evaluate_gaussian_all(X_c, x):
       totalvalue += prob
   return totalvalue
 
-
+###import the data from the train set and subdivide it into train and validation; randomize the data 
 train_labels = pd.read_csv("train_labels.csv", index_col="id")
 species_labels = sorted(train_labels.columns.unique())
 class_num=np.size(species_labels)
@@ -37,8 +39,10 @@ y = data[1]
 x_train, x_validate, y_train, y_validate = train_test_split(
     x, y, stratify=y, test_size=0.01
 )
+###limit the data to 3000 of training because it is a very slow method
 x_train=x_train[1:3000]
 y_train=y_train[1:3000]
+###Importing the test set and randomize it
 test = pd.read_csv('features/features_VGG16_test.csv')
 test = np.array(test)
 m, n = test.shape
@@ -51,17 +55,14 @@ x_test = s.fit_transform(x)
 y_test = test[1] 
 x_test=np.array(x_test)
 y_test=np.array(y_test)
+###limit the data to 300 of test because it is a very slow method
 y_test = y_test[0:300]
 y_test=y_test.astype(int)
-
-
-
-
 
 predictions = []
 p_array=[]
 pdfs=[]
-
+###evaluating for each point of the test set and for each class rthe probability of the test point belonging to a gaussian of that class, it also chooses the best class for each point by maximizing the prediction value
 #for i in tqdm(range(x_validate.shape[0])):
 for i in tqdm(range(x_test.shape[0])):
   pdfs = []
@@ -74,14 +75,15 @@ for i in tqdm(range(x_test.shape[0])):
   pred=np.argmax(pdfs)
   predictions.append(pred)
 
-print(predictions)
-# Evaluate the accuracy of the classifier
+
+### Evaluate the accuracy of the classifier
 #accuracy = np.mean(predictions == y_validate)
 accuracy = np.mean(predictions == y_test)
 print(f'Accuracy: {accuracy:.2f}')
 
+###computing the confusion matrix
 confMatrix = metrics.confusion_matrix(y_test, predictions, normalize = None)
 display = metrics.ConfusionMatrixDisplay(confusion_matrix = confMatrix)
 display.plot()
 plt.show()
-plt.title('Confusion Matrix - LogisticRegression')
+plt.title('Confusion Matrix - Parzen Window')
--- Maria_test/svm.py
+++ Maria_test/svm.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
 
-##IMPORTING THE TRAIN DATASET
+###import the data from the train set and subdivide it into train and validation; randomize the data 
 data = pd.read_csv('features/features_VGG16_train.csv')
 data = np.array(data)
 m, n = data.shape
@@ -34,38 +34,25 @@ y_train = y_train[0:7599]
 y_validate = y_validate[0:1900]
 y_train=y_train.astype(int)
 y_validate=y_validate.astype(int)
-print(len(x_train))
-print(y_train)
-print(len(x_validate))
-print(y_validate)
 
-###FINDING THE BEST C VALUE FOR THE TRAIN DATA
-# create an SVM classifier
+###Finding the best c value for the train set and the validation set
 clf = svm.SVC(kernel='sigmoid')
-# define the range of C values to test
 C_range = [0.2,0.4,0.6,0.8,1, 2,5, 10, 100, 1000]
-# create a dictionary of parameters to test
 param_grid = dict(C=C_range)
-# create a GridSearchCV object
 grid = GridSearchCV(clf, param_grid, cv=10, scoring='accuracy')
-# fit the GridSearchCV object to the data
 grid.fit(x_train, y_train)
 predictions = {C: grid.predict(x_validate) for C in C_range}
-# evaluate the accuracy for each value of C on the validation set
 accuracies = {C: accuracy_score(y_validate, predictions[C]) for C in C_range}
-# find the C value that gives the highest accuracy on the validation set
 best_C = max(accuracies, key=accuracies.get)
-# print the best C value and its accuracy on validation set
-print("best c:",grid.best_params_["C"])
+print("best C:",grid.best_params_["C"])
 print("Best C value validation: ", best_C)
-print("Best accuracy on validation set: ", accuracies[best_C])
-###IMPORTIG THE DATASET OF TEST
+
+###Importing the test set and randomize it
 test = pd.read_csv('features/features_VGG16_test.csv')
 test = np.array(test)
 m, n = test.shape
 np.random.shuffle(test)
 test = test.T
-
 s = StandardScaler()
 x = test[2:n].T
 x_test = s.fit_transform(x)
@@ -75,31 +62,22 @@ y_test=np.array(y_test)
 y_test = y_test[0:500]
 y_test=y_test.astype(int)
 
-##TRAINING THE MODEL AND TESTIG IT
-# create SVM classifier
-clf = svm.SVC(kernel='sigmoid', C=accuracies[best_C])
-# wrap classifier in OneVsRestClassifier
+###creating an svm classifier(OneVsRestClassifier) with a sigmoid kernel and the best c value,and evaluating its performance on the test set
+clf = svm.SVC(kernel='sigmoid', C=best_C)
 clf = OneVsRestClassifier(clf)
-
-# train classifier on training data
 clf.fit(x_train, y_train)
-# evaluate classifier on test data
 accuracy = clf.score(x_validate,y_validate)
 accuracy_test=clf.score(x_test,y_test)
 print('Accuracy(%):',accuracy)
 print('Accuracy(%) of test :',accuracy_test)
-
-# generate predictions on the validation set
 predictions = clf.predict(x_validate)
 predictions_2  =clf.predict(x_test)
-# generate classification report
-report = classification_report(y_validate, predictions)
-report=classification_report(y_test,predictions_2)
-print(report)
 
+
+###performing the confusion matrix
 confMatrix = metrics.confusion_matrix(y_test, predictions_2, normalize = None)
 display = metrics.ConfusionMatrixDisplay(confusion_matrix = confMatrix)
 display.plot()
 plt.show()
-plt.title('Confusion Matrix - LogisticRegression')
+plt.title('Confusion Matrix - SVM')
 
